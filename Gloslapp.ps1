6c2bd51c04a82f95aea44f894db28d02d7238983
Johan Dalenius <johan.dalenius@gmail.com>
2023-05-03 13:40:02 +0200

    Proper camelCase

--- Gloslapp.ps1
+++ Gloslapp.ps1
@@ -17,7 +17,7 @@ $allTxtFiles[$last].IsChecked = $true
 
 # Read words from selected files
 $selectedFiles = $allTxtFiles | Where-Object { $_.IsChecked }
-$global:Words = ReadWordsFromTxtFiles($pathTxtFolder, $selectedFiles)
+$global:words = ReadWordsFromTxtFiles($pathTxtFolder, $selectedFiles)
 
 # Pick a word for starters
 $global:currentIndex = $Words[0]
@@ -30,24 +30,24 @@ $global:seenWords = 0
 $PSDefaultParameterValues['*:Encoding'] = 'utf8'
 [xml]$xaml = Get-Content -Path $pathXml
 $reader = New-Object System.Xml.XmlNodeReader $xaml
-$Window = [Windows.Markup.XamlReader]::Load($reader)
+$window = [Windows.Markup.XamlReader]::Load($reader)
 
 # Populate menu to select which textfiles to use 
-$CheckListBox = $Window.FindName("CheckListBox")
-$CheckListBox.ItemsSource = $allTxtFiles
+$checkListBox = $window.FindName("CheckListBox")
+$checkListBox.ItemsSource = $allTxtFiles
 
 # Expander to make the menu drop down 
-$Expander = $Window.FindName("Expander")
+$expander = $window.FindName("Expander")
 #$Expander.Header= "Välj läxor" # <-- issue. encoding problem when setting it from here. reverting to settings header in xaml for now. investigate.
-$Expander.Add_Collapsed({
+$expander.Add_Collapsed({
     $selectedFiles = $allTxtFiles | Where-Object { $_.IsChecked }
-    $global:Words = ReadWordsFromTxtFiles($pathTxtFolder, $selectedFiles)
+    $global:words = ReadWordsFromTxtFiles($pathTxtFolder, $selectedFiles)
     RandomWord
 })
 
 # Text - Glosa
-$Glosa = $Window.FindName("textBlockGlosa")
-$Glosa.Text = $words[$currentIndex]
+$glosa = $window.FindName("textBlockGlosa")
+$glosa.Text = $words[$currentIndex]
 
 # Button - Previous
 $buttonPrev = $Window.FindName("buttonPrev")
@@ -68,7 +68,7 @@ $buttonNext.Add_Click({
 })
 
 # Keyboard bindings
-$Window.Add_KeyDown({
+$window.Add_KeyDown({
     switch ($_.Key) {
         "Up"    { RandomWord }
         "Down"  { RandomWord }
@@ -78,13 +78,13 @@ $Window.Add_KeyDown({
     }
 })
 
-$Window.Add_Closing({
+$window.Add_Closing({
     $seenWords | Out-File -FilePath .\seenWords.txt -Append
     Write-Host "Ord som visats: $seenWords"
 })
 
 #$words
-$Window.ShowDialog() | Out-Null
+$window.ShowDialog() | Out-Null
 
 
 
